f8a36a9b5a63e5523b6923997f974ce29ed10b96
Can Tektaş <ruirlia@hotmail.com>
2023-06-22 14:28:32 +0300

    HR1

---
+++ OOPS.WebUI/Controllers/MyProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OOPS.DTO.Employee;
+using OOPS.DTO.ProjectBase;
+
+namespace OOPS.WebUI.Controllers
+{
+    public class MyProfileController : BaseController
+    {
+        public MyProfileController()
+        {
+           
+        }
+        public IActionResult Index()
+        {
+            return View(CurrentUser);
+        }
+    }
+
+
+}
